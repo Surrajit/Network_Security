1c1039ccf079c48222a47d35ecee11eb5a566d4c
Surrajit <152585200+Surrajit@users.noreply.github.com>
2024-11-30 19:32:04 +0530

    app file uploaded

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -7,7 +7,7 @@ from networksecurity.logging.logger import logging
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
-
+from networksecurity.components.model_trainer import ModelTrainer
 
 from networksecurity.entity.config_entity import(
     TrainingPipelineConfig,
@@ -24,16 +24,13 @@ from networksecurity.entity.artifact_entity import (
     ModelTrainerArtifact,
 )
 
-from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
-
-from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR
 import sys
 
 
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config=TrainingPipelineConfig()
-        
+
         
 
     def start_data_ingestion(self):
@@ -67,4 +64,42 @@ class TrainingPipeline:
             data_transformation_artifact = data_transformation.initiate_data_transformation()
             return data_transformation_artifact
         except Exception as e:
-            raise NetworkSecurityException(e,sys)        
+            raise NetworkSecurityException(e,sys)
+        
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
+        try:
+            self.model_trainer_config: ModelTrainerConfig = ModelTrainerConfig(
+                training_pipeline_config=self.training_pipeline_config
+            )
+
+            model_trainer = ModelTrainer(
+                data_transformation_artifact=data_transformation_artifact,
+                model_trainer_config=self.model_trainer_config,
+            )
+
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+
+        
+    
+    
+    def run_pipeline(self):
+        try:
+            data_ingestion_artifact=self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
+            
+            return model_trainer_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    
